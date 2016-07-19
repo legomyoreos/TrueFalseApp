3ff6d6ab0b669cd09498f65d1478d16077886b49
legomyoreos <martinez.webdev@gmail.com>
2016-07-19 19:35:26 -0400

    completed struct with all questions. changed code so random question will display when app opens

--- TrueFalseStarter/QuestionsModel.swift
+++ TrueFalseStarter/QuestionsModel.swift
@@ -17,6 +17,14 @@
 struct QuestionsModel {
     let question = [
         ["Question": "This was the only US President to serve more than two terms", "OptionOne": "George Washington", "OptionTwo": "Franklin D. Roosevelt", "OptionThree": "Woodrow Wilson", "OptionFour": "Andrew Jackson", "Answer": "OptionTwo"],
-        ["Question": "Which of the following coutnries has the most residents?", "Answer": "OptionFour"]
+        ["Question": "Which of the following coutnries has the most residents?", "OptionOne": "Nigeria", "OptionTwo": "Russia", "OptionThree": "Iran", "OptionFour": "Vietnam", "Answer": "OptionOne"],
+        ["Question": "In what year was the United Nations founded?", "OptionOne": "1918", "OptionTwo": "1919", "OptionThree": "1945", "OptionFour": "1954", "Answer": "OptionThree"],
+        ["Question": "The Titanic departed from the United Kingdom, where was it supposed to arrive?", "OptionOne": "Paris", "OptionTwo": "Washington D.C", "OptionThree": "New York City", "OptionFour": "Boston", "Answer": "OptionThree"],
+        ["Question": "Which nation produces the most oil?", "OptionOne": "Iran", "OptionTwo": "Iraq", "OptionThree": "Brazil", "OptionFour": "Canada", "Answer": "OptionFour"],
+        ["Question": "Which country has most recently won consecutive World Cups in Soccer?", "OptionOne": "Italy", "OptionTwo": "Brazil", "OptionThree": "Argentina", "OptionFour": "Spain", "Answer": "OptionTwo"],
+        ["Question": "Which of the following rivers is longest?", "OptionOne": "Yangtze", "OptionTwo": "Mississippi", "OptionThree": "Congo", "OptionFour": "Mekong", "Answer": "OptionTwo"],
+        ["Question": "Which city is oldest?", "OptionOne": "Mexico City", "OptionTwo": "Cape Town", "OptionThree": "San Juan", "OptionFour": "Sydney", "Answer": "OptionOne"],
+        ["Question": "Which country was the first to allow women to vote in national elections?", "OptionOne": "Polan", "OptionTwo": "United States", "OptionThree": "Sweden", "OptionFour": "Senegal", "Answer": "OptionOne"],
+        ["Question": "Which of these countries won the most medals in 2012 Summer Games?", "OptionOne": "France", "OptionTwo": "Germany", "OptionThree": "Japan", "OptionFour": "Great Britain", "Answer": "OptionFour"]
     ]
 }
--- TrueFalseStarter/ViewController.swift
+++ TrueFalseStarter/ViewController.swift
@@ -16,19 +16,17 @@ class ViewController: UIViewController {
     var questionsAsked = 0
     var correctQuestions = 0
     var indexOfSelectedQuestion: Int = 0
+    var usedQuestions = []
+    
+    let questionsModel = QuestionsModel()
     
     var gameSound: SystemSoundID = 0
     
-    let trivia: [[String : String]] = [
-        ["Question": "Only female koalas can whistle", "Answer": "False"],
-        ["Question": "Blue whales are technically whales", "Answer": "True"],
-        ["Question": "Camels are cannibalistic", "Answer": "False"],
-        ["Question": "All ducks are birds", "Answer": "True"]
-    ]
-    
     @IBOutlet weak var questionField: UILabel!
-    @IBOutlet weak var trueButton: UIButton!
-    @IBOutlet weak var falseButton: UIButton!
+    @IBOutlet weak var optionOneButton: UIButton!
+    @IBOutlet weak var optionTwoButton: UIButton!
+    @IBOutlet weak var optionThreeButton: UIButton!
+    @IBOutlet weak var optionFourbutton: UIButton!
     @IBOutlet weak var playAgainButton: UIButton!
     
 
@@ -46,76 +44,76 @@ class ViewController: UIViewController {
     }
     
     func displayQuestion() {
-        indexOfSelectedQuestion = GKRandomSource.sharedRandom().nextIntWithUpperBound(trivia.count)
-        let questionDictionary = trivia[indexOfSelectedQuestion]
+        indexOfSelectedQuestion = GKRandomSource.sharedRandom().nextIntWithUpperBound(questionsModel.question.count)
+        let questionDictionary = questionsModel.question[indexOfSelectedQuestion]
         questionField.text = questionDictionary["Question"]
         playAgainButton.hidden = true
     }
     
-    func displayScore() {
-        // Hide the answer buttons
-        trueButton.hidden = true
-        falseButton.hidden = true
-        
-        // Display play again button
-        playAgainButton.hidden = false
-        
-        questionField.text = "Way to go!\nYou got \(correctQuestions) out of \(questionsPerRound) correct!"
-        
-    }
-    
-    @IBAction func checkAnswer(sender: UIButton) {
-        // Increment the questions asked counter
-        questionsAsked += 1
-        
-        let selectedQuestionDict = trivia[indexOfSelectedQuestion]
-        let correctAnswer = selectedQuestionDict["Answer"]
-        
-        if (sender === trueButton &&  correctAnswer == "True") || (sender === falseButton && correctAnswer == "False") {
-            correctQuestions += 1
-            questionField.text = "Correct!"
-        } else {
-            questionField.text = "Sorry, wrong answer!"
-        }
-        
-        loadNextRoundWithDelay(seconds: 2)
-    }
-    
-    func nextRound() {
-        if questionsAsked == questionsPerRound {
-            // Game is over
-            displayScore()
-        } else {
-            // Continue game
-            displayQuestion()
-        }
-    }
-    
-    @IBAction func playAgain() {
-        // Show the answer buttons
-        trueButton.hidden = false
-        falseButton.hidden = false
-        
-        questionsAsked = 0
-        correctQuestions = 0
-        nextRound()
-    }
+//    func displayScore() {
+//        // Hide the answer buttons
+//        trueButton.hidden = true
+//        falseButton.hidden = true
+//        
+//        // Display play again button
+//        playAgainButton.hidden = false
+//        
+//        questionField.text = "Way to go!\nYou got \(correctQuestions) out of \(questionsPerRound) correct!"
+//        
+//    }
+//    
+//    @IBAction func checkAnswer(sender: UIButton) {
+//        // Increment the questions asked counter
+//        questionsAsked += 1
+//        
+//        let selectedQuestionDict = trivia[indexOfSelectedQuestion]
+//        let correctAnswer = selectedQuestionDict["Answer"]
+//        
+//        if (sender === trueButton &&  correctAnswer == "True") || (sender === falseButton && correctAnswer == "False") {
+//            correctQuestions += 1
+//            questionField.text = "Correct!"
+//        } else {
+//            questionField.text = "Sorry, wrong answer!"
+//        }
+//        
+//        loadNextRoundWithDelay(seconds: 2)
+//    }
+//    
+//    func nextRound() {
+//        if questionsAsked == questionsPerRound {
+//            // Game is over
+//            displayScore()
+//        } else {
+//            // Continue game
+//            displayQuestion()
+//        }
+//    }
+//    
+//    @IBAction func playAgain() {
+//        // Show the answer buttons
+//        trueButton.hidden = false
+//        falseButton.hidden = false
+//        
+//        questionsAsked = 0
+//        correctQuestions = 0
+//        nextRound()
+//    }
     
 
     
     // MARK: Helper Methods
     
-    func loadNextRoundWithDelay(seconds seconds: Int) {
-        // Converts a delay in seconds to nanoseconds as signed 64 bit integer
-        let delay = Int64(NSEC_PER_SEC * UInt64(seconds))
-        // Calculates a time value to execute the method given current time and delay
-        let dispatchTime = dispatch_time(DISPATCH_TIME_NOW, delay)
-        
-        // Executes the nextRound method at the dispatch time on the main queue
-        dispatch_after(dispatchTime, dispatch_get_main_queue()) {
-            self.nextRound()
-        }
-    }
+//    func loadNextRoundWithDelay(seconds seconds: Int) {
+//        // Converts a delay in seconds to nanoseconds as signed 64 bit integer
+//        let delay = Int64(NSEC_PER_SEC * UInt64(seconds))
+//        // Calculates a time value to execute the method given current time and delay
+//        let dispatchTime = dispatch_time(DISPATCH_TIME_NOW, delay)
+//        
+//        // Executes the nextRound method at the dispatch time on the main queue
+//        dispatch_after(dispatchTime, dispatch_get_main_queue()) {
+//            self.nextRound()
+//        }
+//    }
     
     func loadGameStartSound() {
         let pathToSoundFile = NSBundle.mainBundle().pathForResource("GameSound", ofType: "wav")
